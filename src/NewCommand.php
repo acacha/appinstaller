56ecb65e4ce510ee2a5b906e63b3b7554413b8ee
Sergi Tur <sergiturbadenas@gmail.com>
2015-02-20 10:23:10 +0100

    Initial commit

---
+++ src/NewCommand.php
@@ -0,0 +1,157 @@
+<?php namespace BootstrapApp\Installer\Console;
+
+use ZipArchive;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class NewCommand extends \Symfony\Component\Console\Command\Command {
+
+	/**
+	 * Configure the command options.
+	 *
+	 * @return void
+	 */
+	protected function configure()
+	{
+		$this->setName('new')
+				->setDescription('Create a new Bootstrap-app application.')
+				->addArgument('type', InputArgument::REQUIRED)
+				->addArgument('name', InputArgument::REQUIRED);
+	}
+
+	/**
+	 * Execute the command.
+	 *
+	 * @param  InputInterface  $input
+	 * @param  OutputInterface  $output
+	 * @return void
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$type = $input->getArgument('type');
+
+		$this->verifyApplicationDoesntExist(
+			$directory = getcwd().'/'.$input->getArgument('name'),
+			$output
+		);
+
+		switch ($type) {
+		    case "laravel":
+		        $output->writeln('<info>Type: laravel</info>');
+		        break;
+		    case "codeigniter":
+		        $output->writeln('<info>Type: codeigniter</info>');
+		        break;
+		    default:
+		    	$output->writeln('<error>Unknown type!</error>');
+		        die(-1);
+		}     
+
+		$output->writeln('<info>Crafting application...</info>');
+
+		$this->download($zipFile = $this->makeFilename(),$type)
+             ->extract($zipFile, $directory)
+             ->cleanUp($zipFile);
+
+		$output->writeln('<comment>Application ready! Build something amazing.</comment>');
+	}
+
+	/**
+	 * Verify that the application does not already exist.
+	 *
+	 * @param  string  $directory
+	 * @return void
+	 */
+	protected function verifyApplicationDoesntExist($directory, OutputInterface $output)
+	{
+		if (is_dir($directory))
+		{
+			$output->writeln('<error>Application already exists!</error>');
+
+			exit(1);
+		}
+
+		if (is_file($directory))
+		{
+			$output->writeln('<error>A file already exists with choosed app name!</error>');
+
+			exit(1);
+		}
+	}
+
+	/**
+	 * Generate a random temporary filename.
+	 *
+	 * @return string
+	 */
+	protected function makeFilename()
+	{
+		return getcwd().'/bootstrap-app_'.md5(time().uniqid()).'.zip';
+	}
+
+	/**
+	 * Download the temporary Zip to the given file.
+	 *
+	 * @param  string  $zipFile
+	 * @return $this
+	 */
+	protected function download($zipFile, $type)
+	{
+		$laravel_url = 'http://acacha.org/boostrap-app/laravel/latest.zip';
+		$codeigniter_url = 'http://acacha.org/boostrap-app/codeigniter/latest.zip';
+
+		$url = "";
+		switch ($type) {
+		    case "laravel":
+		        $url = $laravel_url;
+		        break;
+		    case "codeigniter":
+		        $url = $codeigniter_url;
+		        break;
+		}   
+		
+		$response = \GuzzleHttp\get($url)->getBody();
+
+		file_put_contents($zipFile, $response);
+
+		return $this;
+	}
+
+	/**
+	 * Extract the zip file into the given directory.
+	 *
+	 * @param  string  $zipFile
+	 * @param  string  $directory
+	 * @return $this
+	 */
+	protected function extract($zipFile, $directory)
+	{
+		$archive = new ZipArchive;
+
+		$archive->open($zipFile);
+
+		$archive->extractTo($directory);
+
+		$archive->close();
+
+		return $this;
+	}
+
+	/**
+	 * Clean-up the Zip file.
+	 *
+	 * @param  string  $zipFile
+	 * @return $this
+	 */
+	protected function cleanUp($zipFile)
+	{
+		@chmod($zipFile, 0777);
+
+		@unlink($zipFile);
+
+		return $this;
+	}
+
+}
